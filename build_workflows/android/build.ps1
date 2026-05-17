cb26917df6ec0be931951e1b9c7ab43892dfec35
tsctsc6 <tsctsc6@126.com>
2026-05-17 10:27:05 +0800

    Update android build workflow: build android-arm64 only.

--- build_workflows/android/build.ps1
+++ build_workflows/android/build.ps1
@@ -1,3 +1,5 @@
 $ErrorActionPreference = 'Stop'
 
-flutter build apk --release --split-per-abi -v
+flutter build apk --release --target-platform android-arm64 --split-per-abi -v
+
+# Platforms: android-arm64, android-arm, android-x64
