1974bf064720ebf6164ed5b39068be3c53ea7ef8
tsctsc6 <tsctsc6@126.com>
2026-08-30 14:41:20 +0800

    update flutter_rust_bridge_codegen code

--- lib/src/rust/clean_error.freezed.dart
+++ lib/src/rust/clean_error.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'clean_error.dart';
@@ -9,6 +9,7 @@ part of 'clean_error.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 /// @nodoc
@@ -25,16 +26,21 @@ $CleanErrorCopyWith<CleanError> get copyWith => _$CleanErrorCopyWithImpl<CleanEr
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is CleanError&&(identical(other.message, message) || other.message == message));
+  final _this = this as CleanError;
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is CleanError&&(identical(other.message, _this.message) || other.message == _this.message));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,message);
+int get hashCode {
+  final _this = this as CleanError;
+  return Object.hash(runtimeType,_this.message);
+}
 
 @override
 String toString() {
-  return 'CleanError(message: $message)';
+  final _this = this as CleanError;
+  return 'CleanError(message: ${_this.message})';
 }
 
 
@@ -63,9 +69,8 @@ class _$CleanErrorCopyWithImpl<$Res>
 /// Create a copy of CleanError
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? message = null,}) {
-  return _then(_self.copyWith(
-message: null == message ? _self.message : message // ignore: cast_nullable_to_non_nullable
-as String,
+  return _then(CleanError._(
+
   ));
 }
 
@@ -215,16 +220,18 @@ $CleanError_AnyhowErrorCopyWith<CleanError_AnyhowError> get copyWith => _$CleanE
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is CleanError_AnyhowError&&(identical(other.message, message) || other.message == message));
+    return identical(this, other) || (other.runtimeType == runtimeType&&other is CleanError_AnyhowError&&(identical(other.message, message) || other.message == message));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,message);
+int get hashCode {
+    return Object.hash(runtimeType,message);
+}
 
 @override
 String toString() {
-  return 'CleanError.anyhowError(message: $message)';
+    return 'CleanError.anyhowError(message: $message)';
 }
 
 
