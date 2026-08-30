800e973fc0fdf1c2e2c87599bf396479010ee181
tsctsc6 <tsctsc6@126.com>
2026-08-30 19:38:02 +0800

    fix: arch build

--- build_workflows/arch-x86/build.ps1
+++ build_workflows/arch-x86/build.ps1
@@ -1,6 +1,8 @@
+$ErrorActionPreference = 'Stop'
+$buildWorkflowsPath = "./build_workflows/arch-x86"
+$distPath = "./build/linux/x64/release/"
+
 flutter build linux --release -v
 
-cp -Path "./build_workflows/arch-x86/pwdgenf.desktop.bak" -Destination "./build/linux/x64/release/pwdgenf.desktop.bak"
-cp -Path "./build_workflows/arch-x86/PKGBUILD" -Destination "./build/linux/x64/release/PKGBUILD"
-
-makepkg -f
+Copy-Item -Path "$buildWorkflowsPath/pwdgenf.desktop.bak" -Destination "$distPath/pwdgenf.desktop.bak"
+Copy-Item -Path "$buildWorkflowsPath/PKGBUILD" -Destination "$distPath/PKGBUILD"
